02612a9209ff49ce3b51c73a6194e87017194472
Luis Betancourt <webdeveloperpr@gmail.com>
2020-10-13 20:13:01 -0400

    Update wsgi file and update gitignore

--- app/app/asgi.py
+++ app/app/asgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
+"""
+ASGI entrypoint. Configures Django and then runs the application
+defined in the ASGI_APPLICATION setting.
+"""
 import os
+import django
+from channels.routing import get_default_application
 
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
+django.setup()
+application = get_default_application()
